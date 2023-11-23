071840f126706d0398f49d0656d0f2f062849306
Rizhul Januar <rayjam66@gmail.com>
2023-11-23 09:37:16 +0700

    clerk auth

---
+++ app/(platform)/protected/page.tsx
@@ -0,0 +1,13 @@
+import { UserButton, currentUser } from "@clerk/nextjs";
+
+const ProtactedPage = async () => {
+    return (
+        <div>
+            <UserButton 
+                afterSignOutUrl="/"
+            />
+        </div>
+    );
+};
+
+export default ProtactedPage;
